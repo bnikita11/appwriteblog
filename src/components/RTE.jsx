7b458f3b70c274532a7cf45a032316e00c04c6a3
Nikita Baniya <nikitabaniya57@gmail.com>
2025-04-18 22:09:42 +0545

    removed paste from plugin

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -24,7 +24,9 @@ export default function RTE({name,control,label,defaultValue=""}) {
             plugins:[
                 "advlist", "autolink", "lists", "link", "image", "charmap", "print", "preview", "anchor",
                 "searchreplace", "visualblocks", "code", "fullscreen",
-                "insertdatetime", "media", "table", "paste", "code", "help", "wordcount",
+                "insertdatetime", "media", "table", 
+                // "paste", 
+                "code", "help", "wordcount",
             ],
             toolbar:"undo redo | formatselect | bold italic backcolor | \
             alignleft aligncenter alignright alignjustify | \
